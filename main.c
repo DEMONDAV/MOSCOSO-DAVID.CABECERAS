fef23890e65d9cd86e4f2827c6a0b2e0a68b106f
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 11:42:11 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+
+
+{
+    int a,  b;
+    printf("ingrese 1er numero");
+    scanf("%d",&a);
+    printf("ingrese 2do numero");
+    scanf("%d",&b);
+    sumar (a,b);
+}
