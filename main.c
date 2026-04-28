012772953b104ff7cce8d0a77f4bb7561da65eab
Laboratorio <laboratorio@udla-ec.int>
2026-04-28 12:07:59 -0500

    third commit

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "sumar.h"
+
 int main ()
-
-
 {
     int a,  b;
     printf("ingrese 1er numero");
     scanf("%d",&a);
     printf("ingrese 2do numero");
     scanf("%d",&b);
-    sumar (a,b);
+    printf ("la suma es: %d",sumar (a,b));
+
+    return 0;
 }
